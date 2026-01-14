c00591b15d10a950c95b65eb1a2e8fe4db09d3cd
Dmitry Draganov <draqanovdfeet@gmail.com>
2026-01-14 10:50:50 +0300

    init

---
+++ src/app/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<></>} />
+      <Route path='*' element={<></>} />
+    </Routes>
+  );
+};
+
+export default Router;
