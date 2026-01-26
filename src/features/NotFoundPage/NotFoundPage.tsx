b409ba7d8cefc4258dc94c178fa78eb874f52b52
Dmitry Draganov <draqanovdfeet@gmail.com>
2026-01-26 17:42:45 +0300

    move maint.tsx and css

--- src/features/NotFoundPage/NotFoundPage.tsx
+++ src/features/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 const NotFoundPage = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto text-foreground'>
       <h1 className='text-5xl font-bold'>404 - Страница не найдена</h1>
     </div>
   );
